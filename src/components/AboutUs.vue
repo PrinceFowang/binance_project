61960a2e46edc32a6f167a28de6ffad8b5a2a1d8
Prince FOWANG <berthaultfowang8@gmail.com>
2023-10-29 12:01:02 +0100

    about

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,31 +1,83 @@
 <template>
+  <header-component></header-component>
   <div class="about-us">
-    <header-component></header-component>
-    <transition name="fade" mode="out-in">
-      <div class="about-card card mb-3">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img
-              src="/path/to/your/image.jpg"
-              class="img-fluid rounded-start"
-              alt="Company Image"
-            />
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">About Our Company</h5>
-              <p class="card-text">
-                Welcome to Gorilla Wealth, your trusted partner for financial
-                services and wealth management solutions.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
+    <div>
+      <h1>Our team</h1>
+      <p>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </p>
+    </div>
+    <div class="centered-cards">
+      <div class="card mb-2 cart" style="max-width: 400px; margin: 10px">
+        <div class="card-content">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img
+                src="@/assets/Prince.jpg"
+                class="img-fluid rounded-start"
+                alt="PrincePicture"
+                style="max-width: 300%; height: auto"
+              />
+            </div>
+            <div class="col-md-15">
+              <div class="card-body">
+                <h5 class="card-title">Prince FOWANG</h5>
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <a
+                  href="https://www.linkedin.com/in/prince-fowang-4a34a2251/"
+                  target="_blank"
+                >
+                  <img
+                    src="@/assets/LinkedIn-logo.webp"
+                    alt="LinkedIn"
+                    style="max-width: 15%; height: auto"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </transition>
+      <div class="card mb-2 cart" style="max-width: 400px; margin: 10px">
+        <div class="card-content">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img
+                src="@/assets/Pierre.jpg"
+                class="img-fluid rounded-start"
+                alt="PrincePicture"
+                style="max-width: 300%; height: auto"
+              />
+            </div>
+            <div class="col-md-15">
+              <div class="card-body">
+                <h5 class="card-title">Pierre BENJAMIN</h5>
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <a
+                  href="https://www.linkedin.com/in/nathana%C3%ABl-benjamin-500699136/details/education/"
+                  target="_blank"
+                >
+                  <img
+                    src="@/assets/LinkedIn-logo.webp"
+                    alt="LinkedIn"
+                    style="max-width: 15%; height: auto"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,29 +95,46 @@ export default {
 <style scoped>
 .about-us {
   display: flex;
+  align-items: center; /* Vertically center the cards */
+  justify-content: center; /* Horizontally center the cards */
   height: 100vh;
   background: linear-gradient(to right, #4e4d4d, #1f1f1f);
 }
+.centered-cards {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  text-align: center;
+  margin-top: 20px;
+}
 
-.about-card {
-  max-width: 540px;
-  background: #fff;
+.cart {
+  border-color: #1f1f1f;
+}
+
+h1 {
+  color: #eebd50;
+  position: relative;
+  right: 15%;
+  top: -30%;
+}
+
+.card-content {
+  background: rgba(78, 77, 77, 0.9); /* Slightly lighter background color */
+  padding: 20px;
   border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease; /* Add a smooth transition effect */
+  transition: background 0.3s; /* Smooth transition for background color */
 }
 
-.about-card:hover {
-  transform: scale(1.05); /* Scale up the card on hover */
+.card-title {
+  color: #eebd50;
 }
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s; /* Add a fade-in/out transition effect */
+.card-text {
+  color: white;
 }
 
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
+.card-content:hover {
+  background: rgba(78, 77, 77, 0.7); /* Lighter background on hover */
 }
 </style>
